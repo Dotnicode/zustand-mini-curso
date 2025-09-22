9be42c6ec09d1f5054b7c9f448813ecaf05dbf72
Nico Olmos <romero.nico18@gmail.com>
2025-09-22 16:34:58 +0000

    Refactor JiraTasks component to use custom useTasks hook for task management; implement useTasks hook for handling task state and drag events

--- src/components/jira/JiraTasks.tsx
+++ src/components/jira/JiraTasks.tsx
@@ -1,10 +1,8 @@
 import classNames from "classnames";
-import { DragEvent, useState } from "react";
 import { IoAddOutline, IoCheckmarkCircleOutline } from "react-icons/io5";
+import { useTasks } from "../../hooks/useTasks";
 import { Task, TaskStatus } from "../../interfaces";
-import { useTaskStore } from "../../stores";
 import { SingleTask } from "./SingleTask";
-import Swal from "sweetalert2";
 
 interface Props {
   title: string;
@@ -13,41 +11,14 @@ interface Props {
 }
 
 export const JiraTasks = ({ title, status, tasks }: Props) => {
-  const isDragging = useTaskStore((state) => !!state.dragginTaskId);
-  const onTaskDrop = useTaskStore((state) => state.onTaskDrop);
-  const addTask = useTaskStore((state) => state.addTask);
-
-  const [onDragOver, setOnDragOver] = useState(false);
-
-  const handleAddTask = async () => {
-    const { isConfirmed, value } = await Swal.fire({
-      title: "Nueva tarea",
-      input: "text",
-      inputLabel: "Nombre de la tarea",
-      inputPlaceholder: "Ingrese el nombre de la tarea",
-      showCancelButton: true,
-      inputValidator: (value) => {
-        if (!value) return "Debe de ingresar un nombre para la tarea";
-      },
-    });
-
-    if (!isConfirmed) return;
-    addTask(value, status);
-  };
-
-  const handleDragOver = (event: DragEvent<HTMLDivElement>) => {
-    event.preventDefault();
-    setOnDragOver(true);
-  };
-  const handleDragLeave = (event: DragEvent<HTMLDivElement>) => {
-    event.preventDefault();
-    setOnDragOver(false);
-  };
-  const handleDrop = (event: DragEvent<HTMLDivElement>) => {
-    event.preventDefault();
-    setOnDragOver(false);
-    onTaskDrop(status);
-  };
+  const {
+    isDragging,
+    onDragOver,
+    handleAddTask,
+    handleDragLeave,
+    handleDragOver,
+    handleDrop,
+  } = useTasks({ status });
 
   return (
     <div
