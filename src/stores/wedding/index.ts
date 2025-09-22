c070cbcb389aa2e362f2c6d683693337a0c86f9a
Nico Olmos <romero.nico18@gmail.com>
2025-09-22 18:44:12 +0000

    Add guest slice to manage guest count; update WeddingInvitationPage to handle guest count input

--- src/stores/wedding/index.ts
+++ src/stores/wedding/index.ts
@@ -1,11 +1,13 @@
 import { create } from "zustand";
-import { createPersonSlice, PersonSlice } from "./person.slice";
 import { devtools } from "zustand/middleware";
+import { createGuestSlice, GuestSlice } from "./guest.slice";
+import { createPersonSlice, PersonSlice } from "./person.slice";
 
-type ShareState = PersonSlice;
+type ShareState = PersonSlice & GuestSlice;
 
 export const useWeddingBoundStore = create<ShareState>()(
   devtools((...a) => ({
     ...createPersonSlice(...a),
+    ...createGuestSlice(...a),
   }))
 );
